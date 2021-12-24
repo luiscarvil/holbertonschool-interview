386e6e7ce85f6e0ef79f3f2ce9ef54a315f19fb4
luiscarvil <luiscarvajal05@gmail.com>
2021-12-23 20:18:22 -0500

    fixed

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,4 +1,14 @@
 #include "binary_trees.h"
+
+/**
+ * heap_insert - Creates a binary tree node
+ *
+ * @parent: Pointer to the parent node of the node to create
+ * 
+ * @value:  Value to put in the new node
+ * 
+ * Return: A tree with max heap ordering
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
 
     binary_tree_t *new = NULL;
--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,10 +1,19 @@
 #include "binary_trees.h"
 
+/**
+ * heap_insert - Inserts a value into a Max Binary Heap
+ *
+ * @root: Double pointer to the root node of the Heap
+ * 
+ * @value: The value store in the node to be inserted
+ * 
+ * Return: A tree with max heap 
+ */
+
 heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *node, *parent;
 
-	/* For an empty tree, create nodes and return tree pointer to tree */
 	if (*root == NULL)
 	{
 		node = binary_tree_node(*root, value);
@@ -12,34 +21,27 @@ heap_t *heap_insert(heap_t **root, int value)
 		return (*root);
 	}
 	node = *root;
-
-	/* Traverse tree */
 	while (node != NULL)
 	{
 		parent = node;
 		/* Ignore duplicates */
 		if (node->n == value){
-            	while (node && node->parent)
+            while (node && node->parent)
+                {
+                    while (node->n > node->parent->n)
                     {
-                        while (node->n > node->parent->n)
-                        {
-                            node = node->parent;
-                            node->n += node->parent->n;
-                            node->parent->n -= node->n;
-                            node->parent->n = node->n - node->parent->n;
-                        }
+                        node = node->parent;
+                        node->n += node->parent->n;
+                        node->parent->n -= node->n;
+                        node->parent->n = node->n - node->parent->n;
                     }
+                }
         }
-			
-
-		/* Sort tree into BST formation */
 		if (value > node->n)
 			node = parent->left;
 		else
 			node = node->right;
 	}
-
-	/* Sort parent nodes */
 	if (value > parent->n)
 	{
 		parent->left = binary_tree_node(parent, value);
