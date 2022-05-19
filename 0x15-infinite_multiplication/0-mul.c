4d7427fd9b3ddc5338d0253272b676d90b66dd94
luiscarvil <luiscarvajal05@gmail.com>
2022-05-18 19:36:18 -0500

    Add mult file

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,94 +1,93 @@
 #include "holberton.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * is_digit - checks if the argument is a number
- * @c: argument
- * Return: 0 on success, otherwise 1
- **/
-int is_digit(char *c)
+ * print_num - prints an array of integers like a number
+ * @nums3: array of integers
+ * @size: size of the array of integers
+ */
+void print_num(int *nums3, int size)
 {
-	while (*c)
+	int i = 0;
+
+	if (!nums3 && !size)
 	{
-		if (*c < '0' || *c > '9')
-			return (0);
-		c++;
+		printf("0\n");
+		exit(0);
 	}
-	return (1);
-}
-
-/**
- * _strlen - find the length of the a string
- * @s: a string
- * Return: the length of the string
- **/
-int _strlen(char *s)
-{
-	char *ptr = s;
-
-	while (*s)
-		s++;
-	return (s - ptr);
-}
-
-/**
- * multiply - multiplies two numbers
- * @a: first number
- * @b: second number
- * Return: Nothing
- **/
-void multiply(char *a, char *b)
-{
-	int i, len_a, len_b, total, a_number, b_number, res = 0, tmp;
-	int *ptr;
-
-	len_a = _strlen(a);
-	len_b = _strlen(b);
-	tmp = len_b;
-	total = len_a + len_b;
-	ptr = malloc(sizeof(int) * total);
-	if (!ptr)
-		return;
-	for (len_a--; len_a >= 0; len_a--)
+	while (nums3[i] == 0)
+		i++;
+	for (; i < size; i++)
 	{
-		a_number = a[len_a] - '0';
-		res = 0;
-		len_b = tmp;
-		for (len_b--; len_b >= 0; len_b--)
-		{
-			b_number = b[len_b] - '0';
-			res += ptr[len_b + len_a + 1] + (a_number * b_number);
-			ptr[len_a + len_b + 1] = res % 10;
-			res /= 10;
-		}
-		if (res)
-			ptr[len_a + len_b + 1] = res % 10;
+		printf("%d", nums3[i]);
 	}
-	while (*ptr == 0)
-	{
-		ptr++;
-		total--;
-	}
-	for (i = 0; i < total; i++)
-		printf("%i", ptr[i]);
 	printf("\n");
 }
 
 /**
- * main - Entry point, program that multiplies two positive numbers
- * @argc: argument count
- * @argv: argument values
- * Return: 0 on success
- **/
-int main(int argc, char *argv[])
+ * multiply - multiplies two large numbers
+ * @nums1: string representation of large int
+ * @nums2: string representation of large int
+ * Return: 1 on success, 0 on failure
+ */
+int multiply(char *nums1, char *nums2)
 {
-	char *num1 = argv[1];
-	char *num2 = argv[2];
+	int i, j;
+	int len1, len2, sum, n1, n2 = 0;
+	int *nums3;
 
-	if (argc != 3 || !is_digit(num1) || !is_digit(num2))
+	len1 = strlen(nums1);
+	len2 = strlen(nums2);
+
+	nums3 = calloc(len1 + len2, sizeof(len1 + len2));
+	if (!nums3)
+		return (0);
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		n1 = nums1[i] - '0';
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n2 = nums2[j] - '0';
+			sum = (n1 * n2) + nums3[i + j + 1];
+			nums3[i + j] += sum / 10;
+			nums3[i + j + 1] = sum % 10;
+		}
+	}
+	print_num(nums3, len1 + len2);
+	free(nums3);
+	return (1);
+}
+
+/**
+ * main - Entry point: multiplies two positive numbers
+ * @argc: number of arguments passed
+ * @argv: arguments passed (integers)
+ * Return: 0 in success, 1 on failure
+ */
+int main(int argc, char **argv)
+{
+	int i, j = 0;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	multiply(num1, num2);
+	for (i = 1; argv[i]; i++)
+		for (j = 0; argv[i][j]; j++)
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				exit(98);
+
+			}
+
+	if (*argv[1] == '0' || *argv[2] == '0')
+		print_num(NULL, 0);
+	if (!multiply(argv[1], argv[2]))
+		return (1);
 	return (0);
 }
